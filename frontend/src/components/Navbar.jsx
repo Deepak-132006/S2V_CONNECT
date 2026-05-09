c36be23b43506f52b4922898e644ce97fc7b3c57
srimathi-art <srimathipalanisami392@gmail.com>
2026-05-09 11:21:07 +0530

    frontend eh

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Ham from '../assets/hamburger.png'
+
+
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='w-5'>
+    <img src={Ham} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
